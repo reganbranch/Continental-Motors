7dcca06578c24c9f636701176cca683a227f16a8
reganbranch <regan@reganbranch.com>
2018-11-01 16:06:26 -0500

    Can now populate dropdowns with data, use user input data, and reset

--- public/app.js
+++ public/app.js
@@ -7,15 +7,54 @@ const firestore = firebase.firestore();
 const settings = {/* your settings... */ timestampsInSnapshots: true};
 firestore.settings(settings);
 
-var GeneralRef = db.collection("General").doc('BNS2');
+
+const engineBox = document.querySelector("#engine");
+const fastenerBox = document.querySelector("#fastener");
+const sizeBox = document.querySelector("#size");
+const torqueBox = document.querySelector("#torque");
+const clearButton = document.querySelector("#clear");
+
+
+db.collection("TSIO-550K").get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+        // engineBox.innerHTML += "<option>" + doc.dat().size + "</option>";
+        // fastenerBox.innerHTML += "<option>" + doc.data().fastener + "</option>";
+
+    });
+});
+
+engineBox.addEventListener("change", function() {
+  const enginechoice = engineBox.value;
+  console.log(enginechoice);
+})
+
+clearButton.addEventListener("click", function() {
+  engineBox.value = "";
+})
+
+// var TSIORef = db.collection("TSIO-550K");
+// var GeneralRef = db.collection("General");
+// var queryfast1 = TSIORef.where("fastener", "==", true);
+// var queryfast2 = GeneralRef.where("fastener", "==", true);
+// console.log(queryfast1);
+// var select = document.getElementById("fastener");
+//
+// for(var i = 0; i < queryfast1.length; i++) {
+//     var opt = queryfast1[i];
+//     var el = document.createElement("queryfast1");
+//     el.textContent = opt;
+//     el.value = opt;
+//     select.appendChild(el);
+// };
+
+
 // myPost.onSnapshot(doc => {
 //   const data = doc.data();
 //   console.log(data)
 // })
-// GeneralRef.doc("SF").set({
-//     name: "San Francisco", state: "CA", country: "USA",
-//     capital: false, population: 860000,
-//     regions: ["west_coast", "norcal"] });
+
 
 
 // load.addEventListener("click", function() {
@@ -29,14 +68,12 @@ var GeneralRef = db.collection("General").doc('BNS2');
 //   });
 // });
 
+// var GeneralRef = db.collection("General").doc('BNS2');
+// GeneralRef.doc("SF").set({
+//     name: "San Francisco", state: "CA", country: "USA",
+//     capital: false, population: 860000,
+//     regions: ["west_coast", "norcal"] });
 
-db.collection("General").get().then(function(querySnapshot) {
-    querySnapshot.forEach(function(doc) {
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
-        output.innerText = doc.data();
-    });
-});
 
 
 /*
